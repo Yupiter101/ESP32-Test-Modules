0ff660b5b2ef668c6520120ac28b046fd16b1204
Sergey_Sanyuk <shv08@ukr.net>
2024-06-16 19:02:13 +0300

    I2c scaner

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,21 @@
     Завдання:
     - Додати кнопку з Interrupt перемикання між модулями
     - Замінити delay() на millis()
-    - Винести кожний модуль в окремий файл
-    - - Розбити окремі файли на .h та .cpp
+        + Винести кожний модуль в окремий файл
+        + Розбити окремі файли на .h та .cpp
     - Слідкувати за змінами величин від модулів. Якщо ні то помилка
     - Додати звукові сигнали (що усе ок)
-    + Додати модуль BME280
-    + Додати модуль HMC5883
+        + Додати модуль BME280
+        + Додати модуль HMC5883
     - Додати Сканер I2C
     - Додати акселерометр MPU-9250
     - Додати сімкарту
     - Додати роботу з GPS
     - Реалізувати спілкування між двома ESP32 
-    - Залить на GIT
+        + Залить на GIT
+    - Device at address HMC5886 MPU9250  BME280
+    - Device at address  0x1E    0x68     0x76
+    - Device at analog   ----    0x69     ----
 */
 
 
@@ -28,7 +31,8 @@
 
 boolean flag_BME_Init = true;
 boolean flag_HMC_Init = true;
-
+// uint32_t lastMilisBME = 0;
+int i2cArrAdr[5]; // масив адресів
 
 
 void setup() {
@@ -38,7 +42,49 @@ void setup() {
     Wire.begin(I2C_SDA, I2C_SCL); // 33 32
 
 
-    // /* === Initialise HMC5883 sensor == */
+
+    /* === I2C scanner == */
+    byte error, address; //variable for error and I2C address
+    int nDevices = 0;
+    
+
+    Serial.println("Scanning...");
+
+    // nDevices = 0;
+    for (address = 1; address < 127; address++ )
+    {
+      Wire.beginTransmission(address);
+      error = Wire.endTransmission();
+
+      if (error == 0)
+      {
+        Serial.print("Device at address 0x");
+        if (address < 16)  Serial.print("0");
+        Serial.println(address, HEX);
+        i2cArrAdr[nDevices] = address;
+        nDevices++;
+      }
+      else if (error == 4)
+      {
+        Serial.print("Error at address 0x");
+        if (address < 16) Serial.print("0");
+        Serial.println(address, HEX);
+      }
+    }
+    if (nDevices == 0)
+      Serial.println("No I2C devices found\n");
+    else
+      Serial.println("done\n");
+
+    // while(1){
+    //   delay(5); // wait 5 seconds for the next I2C scan
+    // }
+
+
+
+  
+
+    /* === Initialise HMC5883 sensor == */
     flag_HMC_Init = initHMC();
   
     delay(500);
@@ -50,15 +96,22 @@ void setup() {
 
 
 void loop() { 
+
+    
     // == show_BME280 ==
     if(flag_BME_Init) {
         Serial.println("Log BME280");
         for(int i=0; i<10; i++) {
             show_BME280_values();
-            delay(1000);
+            delay(500);
         } 
     }
     delay(2000);
+
+    // if(millis() - lastMilisBME >= 1000){
+    //   lastMilisBME = millis();
+    // }
+
     Serial.println(); 
 
     // == show_HMC5883 ==
@@ -66,7 +119,7 @@ void loop() {
         Serial.println("Log HMC5883");
         for(int i=0; i<10; i++) {
             show_HMC5883_values();
-            delay(1000);
+            delay(500);
         } 
     }
 
