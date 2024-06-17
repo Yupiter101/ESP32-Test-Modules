6024a10416fe3c6c9f70da0d53685a908103d64c
Serhii Saniuk <shv08@ukr.net>
2024-06-17 21:26:31 +0300

    QMC5883

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,19 @@
         + Залить на GIT
     - Device at address HMC5886 MPU9250  BME280  OLED-0.96
     - Device at address  0x1E    0x68     0x76     0x3C
-    - Device at analog   ----    0x69     ----     0x3D
+    - Device at analog   0x0D    0x69     ----     0x3D
+    - Device at dex     30(13)    104     118        60
 */
 
 
 #include <Arduino.h>
 // #include <Wire.h>
+#include <QMC5883LCompass.h>
 #include "bme280.h"
 #include "hmc.h"
 
+
+QMC5883LCompass compass;
 
 #define I2C_SDA 33
 #define I2C_SCL 32
@@ -32,19 +36,25 @@
 boolean flag_BME_Init = false;
 boolean flag_HMC_Init = false;
 // uint32_t lastMilisBME = 0;
-int i2cAdrArr[5] = { 0, 0, 0, 0, 0 }; // масив адресів
+int i2cAdrArr[8] = { 0, 0, 0, 0, 0, 0, 0, 0 }; // масив адресів
 uint8_t modulesStatus = 0b00000000; // інформація про скановані модулі. макс 8
 
-#define MPU_Status modulesStatus & (1<<0)
-#define BME_Status modulesStatus & (1<<1)
-#define HMC_Status modulesStatus & (1<<2)
-#define OLED_Status modulesStatus & (1<<3)
+// #define MPU_Status modulesStatus & (1<<0)
+// #define BME_Status modulesStatus & (1<<1)
+// #define HMC_Status modulesStatus & (1<<2)
+// #define OLED_Status modulesStatus & (1<<3)
+
+#define QMC_Status modulesStatus & (1<<0)
+#define HMC_Status modulesStatus & (1<<1)
+#define OLED_Status modulesStatus & (1<<2)
+#define MPU_Status modulesStatus & (1<<3)
+#define BME_Status modulesStatus & (1<<4)
 
 
 
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT); // Buzzer pin (2)
     delay(1);
     Serial.begin(115200);
     while(!Serial);    // time to get serial running
@@ -70,27 +80,34 @@ void setup() {
         
         i2cAdrArr[nDevices] = address;
         Serial.print(" - ");
+
         switch(address) {
-          case 0x68:
-            modulesStatus |= (1<<0); // Є MPU9250 значить 0 біт true
-            Serial.println("MPU9250");
-            break;
-          case 0x76:
-            modulesStatus |= (1<<1); // Є BME280 значить 1 біт true
-            Serial.println("BME280");
+          
+          case 0x0D:
+          modulesStatus |= (1<<0); // Є QMC5886 значить 0 біт true
+            Serial.println("QMC5886");
             break;
           case 0x1E:
-          modulesStatus |= (1<<2); // Є HMC5886 значить 2 біт true
+          modulesStatus |= (1<<1); // Є HMC5886 значить 1 біт true
             Serial.println("HMC5886");
             break;
           case 0x3C:
-          modulesStatus |= (1<<3); // Є OLED значить 3 біт true
+          modulesStatus |= (1<<2); // Є OLED значить 2 біт true
             Serial.println("OLED-0.96");
+            break;
+          case 0x68:
+            modulesStatus |= (1<<3); // Є MPU9250 значить 3 біт true
+            Serial.println("MPU9250");
+            break;
+          case 0x76:
+            modulesStatus |= (1<<4); // Є BME280 значить 4 біт true
+            Serial.println("BME280");
             break;
           default:
             Serial.println("No inform.");
             break;
         }
+
         nDevices++;
       }
       else if (error == 4)
@@ -103,7 +120,7 @@ void setup() {
     if (nDevices == 0)
       Serial.println("No I2C devices found\n");
     else
-      Serial.println("done\n");
+      Serial.println("I2C done\n");
 
     while(!nDevices){
       digitalWrite(LED_BUILTIN, HIGH);
@@ -112,27 +129,30 @@ void setup() {
       delay(450);
     }
 
+    // compass.init();
+
 
     /* === Initialise HMC5883 sensor == */
-    if(HMC_Status) {
-      Serial.println("Enable HMC5883");
-      flag_HMC_Init = initHMC();
+    if(QMC_Status) {
+      Serial.println("QMC5883 detected");
+      // flag_HMC_Init = initHMC();
+      compass.init();
+      
     }
-    else {
-      Serial.println("Disable HMC5883");
+   
+
+    if(HMC_Status) {
+      Serial.println("HMC5883 detected");
+      flag_HMC_Init = initHMC();
     }
   
     delay(500);
 
     /* === Initialise BME280 sensor === */
     if(BME_Status) {
-      Serial.println("Enable BME280");
+      Serial.println("BME280 detected");
       flag_BME_Init = initBME280();
     }
-    else {
-      Serial.println("Disable BME280");
-    }
-
 }
 
 
@@ -157,7 +177,7 @@ void loop() {
 
     // == show_HMC5883 ==
     if(flag_HMC_Init) {
-        Serial.println("Log HMC5883");
+        Serial.println("Log (Q)HMC5883");
         for(int i=0; i<10; i++) {
             show_HMC5883_values();
             delay(500);
@@ -165,6 +185,27 @@ void loop() {
     }
 
     delay(2000);
+
+
+    if(QMC_Status) {
+      int x, y, z;  
+      // Read compass values
+      compass.read();
+
+      // Return XYZ readings
+      x = compass.getX();
+      y = compass.getY();
+      z = compass.getZ();
+      
+      Serial.print("X: ");
+      Serial.print(x);
+      Serial.print(" Y: ");
+      Serial.print(y);
+      Serial.print(" Z: ");
+      Serial.print(z);
+      Serial.println();
+    }
+
     Serial.println();
 }
 
