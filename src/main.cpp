96a3048c7ee294d9d9e3e6a5530f65e325257ca6
Serhii Saniuk <shv08@ukr.net>
2024-06-19 12:09:07 +0300

    make-mpu-all-data

--- src/main.cpp
+++ src/main.cpp
@@ -28,14 +28,14 @@
 // #include <Wire.h>
 
 #include <MPU9250_WE.h>
-
 #include <QMC5883LCompass.h>
 #include "bme280.h"
 #include "hmc.h"
 
-#define MPU9250_ADDR 0x68
+
 QMC5883LCompass compass;
 // MPU - accelerom, giro, magnetom
+#define MPU9250_ADDR 0x68
 MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR);
 
 #define I2C_SDA 33
@@ -63,14 +63,12 @@ uint8_t modulesStatus = 0b00000000; // Наявні I2C модулі. макс 8
 
 void setup() {
 
-    pinMode(LED_BUILTIN, OUTPUT); // Buzzer pin (2)
-    pinMode(BUTTON_PIN, INPUT_PULLUP); // Btn pin HIGH, wait LOW
-
     Serial.begin(115200);
     while(!Serial);    // time to get serial running
     Wire.begin(I2C_SDA, I2C_SCL); // 33 32
 
-
+    pinMode(LED_BUILTIN, OUTPUT); // Buzzer pin (2)
+    pinMode(BUTTON_PIN, INPUT_PULLUP); // Btn pin HIGH, wait LOW
 
     /* === I2C scanner == */
     byte error, address; //variable for error and I2C address
@@ -165,16 +163,21 @@ void setup() {
 
     /* === Initialise MPU9250 sensor Accereration=== */
     if(MPU_Status) {   // Якщо Є такий I2C адрес то почати ініціалізацію
-
+      
       if(!myMPU9250.init()){
         Serial.println("MPU9250 does not respond");
       }
       else{
         Serial.println("MPU9250 is connected");
       }
+      if(!myMPU9250.initMagnetometer()){
+        Serial.println("Magnetometer does not respond");
+      }
+      else{
+        Serial.println("Magnetometer is connected");
+      }
 
-
-        /* The slope of the curve of acceleration vs measured values fits quite well to the theoretical 
+      /* The slope of the curve of acceleration vs measured values fits quite well to the theoretical 
       * values, e.g. 16384 units/g in the +/- 2g range. But the starting point, if you position the 
       * MPU9250 flat, is not necessarily 0g/0g/1g for x/y/z. The autoOffset function measures offset 
       * values. It assumes your MPU9250 is positioned flat with its x,y-plane. The more you deviate 
@@ -195,14 +198,54 @@ void setup() {
       */
       //myMPU9250.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
 
+      /*  The gyroscope data is not zero, even if you don't move the MPU9250. 
+      *  To start at zero, you can apply offset values. These are the gyroscope raw values you obtain
+      *  using the +/- 250 degrees/s range. 
+      *  Use either autoOffset or setGyrOffsets, not both.
+      */
+      //myMPU9250.setGyrOffsets(45.0, 145.0, -105.0);
+
+      /*  You can enable or disable the digital low pass filter (DLPF). If you disable the DLPF, you 
+      *  need to select the bandwdith, which can be either 8800 or 3600 Hz. 8800 Hz has a shorter delay,
+      *  but higher noise level. If DLPF is disabled, the output rate is 32 kHz.
+      *  MPU9250_BW_WO_DLPF_3600 
+      *  MPU9250_BW_WO_DLPF_8800
+      */
+      myMPU9250.enableGyrDLPF();
+      //myMPU9250.disableGyrDLPF(MPU9250_BW_WO_DLPF_8800); // bandwdith without DLPF
+      
+      /*  Digital Low Pass Filter for the gyroscope must be enabled to choose the level. 
+      *  MPU9250_DPLF_0, MPU9250_DPLF_2, ...... MPU9250_DPLF_7 
+      *  
+      *  DLPF    Bandwidth [Hz]   Delay [ms]   Output Rate [kHz]
+      *    0         250            0.97             8
+      *    1         184            2.9              1
+      *    2          92            3.9              1
+      *    3          41            5.9              1
+      *    4          20            9.9              1
+      *    5          10           17.85             1
+      *    6           5           33.48             1
+      *    7        3600            0.17             8
+      *    
+      *    You achieve lowest noise using level 6  
+      */
+      myMPU9250.setGyrDLPF(MPU9250_DLPF_6);
+
       /*  Sample rate divider divides the output rate of the gyroscope and accelerometer.
       *  Sample rate = Internal sample rate / (1 + divider) 
       *  It can only be applied if the corresponding DLPF is enabled and 0<DLPF<7!
       *  Divider is a number 0...255
       */
       myMPU9250.setSampleRateDivider(5);
-      
-      /*  MPU9250_ACC_RANGE_2G      2 g   
+
+      /*  MPU9250_GYRO_RANGE_250       250 degrees per second (default)
+      *  MPU9250_GYRO_RANGE_500       500 degrees per second
+      *  MPU9250_GYRO_RANGE_1000     1000 degrees per second
+      *  MPU9250_GYRO_RANGE_2000     2000 degrees per second
+      */
+      myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
+
+      /*  MPU9250_ACC_RANGE_2G      2 g   (default)
       *  MPU9250_ACC_RANGE_4G      4 g
       *  MPU9250_ACC_RANGE_8G      8 g   
       *  MPU9250_ACC_RANGE_16G    16 g
@@ -228,32 +271,6 @@ void setup() {
       */
       myMPU9250.setAccDLPF(MPU9250_DLPF_6);
 
-      /*  Set accelerometer output data rate in low power mode (cycle enabled)
-      *   MPU9250_LP_ACC_ODR_0_24          0.24 Hz
-      *   MPU9250_LP_ACC_ODR_0_49          0.49 Hz
-      *   MPU9250_LP_ACC_ODR_0_98          0.98 Hz
-      *   MPU9250_LP_ACC_ODR_1_95          1.95 Hz
-      *   MPU9250_LP_ACC_ODR_3_91          3.91 Hz
-      *   MPU9250_LP_ACC_ODR_7_81          7.81 Hz
-      *   MPU9250_LP_ACC_ODR_15_63        15.63 Hz
-      *   MPU9250_LP_ACC_ODR_31_25        31.25 Hz
-      *   MPU9250_LP_ACC_ODR_62_5         62.5 Hz
-      *   MPU9250_LP_ACC_ODR_125         125 Hz
-      *   MPU9250_LP_ACC_ODR_250         250 Hz
-      *   MPU9250_LP_ACC_ODR_500         500 Hz
-      */
-      //myMPU9250.setLowPowerAccDataRate(MPU9250_LP_ACC_ODR_500);
-
-      /* sleep() sends the MPU9250 to sleep or wakes it up. 
-      * Please note that the gyroscope needs 35 milliseconds to wake up.
-      */
-      //myMPU9250.sleep(true);
-
-    /* If cycle is set, and standby or sleep are not set, the module will cycle between
-      *  sleep and taking a sample at a rate determined by setLowPowerAccDataRate().
-      */
-      //myMPU9250.enableCycle(true);
-
       /* You can enable or disable the axes for gyroscope and/or accelerometer measurements.
       * By default all axes are enabled. Parameters are:  
       * MPU9250_ENABLE_XYZ  //all axes are enabled (default)
@@ -266,14 +283,21 @@ void setup() {
       * MPU9250_ENABLE_000  // all axes disabled
       */
       //myMPU9250.enableAccAxes(MPU9250_ENABLE_XYZ);
+      //myMPU9250.enableGyrAxes(MPU9250_ENABLE_XYZ);
+      
+      /*
+      * AK8963_PWR_DOWN       
+      * AK8963_CONT_MODE_8HZ         default
+      * AK8963_CONT_MODE_100HZ
+      * AK8963_FUSE_ROM_ACC_MODE 
+      */
+      myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
+      delay(200);
+      
 
     }
 
-
-
-    /* === Initialise MPU9250 sensor Giroscop === */
-        // Дивитися з прикладу бібліотеки
-    
+  
 
 }
 
@@ -281,7 +305,7 @@ void setup() {
 void loop() { 
 
     // Тут готувати роботу з кнопкою
-    buttonState = digitalRead(BUTTON_PIN);
+    // buttonState = digitalRead(BUTTON_PIN);
 
 
     // == show_BME280 ==
@@ -343,35 +367,40 @@ void loop() {
     // == show_MPU9250 Accereration ==
     if(MPU_Status) {
       Serial.println("Log MPU9250");
+
       for(int i=0; i<10; i++) {
-        xyzFloat accRaw = myMPU9250.getAccRawValues();
-        xyzFloat accCorrRaw = myMPU9250.getCorrectedAccRawValues();
         xyzFloat gValue = myMPU9250.getGValues();
+        xyzFloat gyr = myMPU9250.getGyrValues();
+        xyzFloat magValue = myMPU9250.getMagValues();
+        float temp = myMPU9250.getTemperature();
         float resultantG = myMPU9250.getResultantG(gValue);
-        
-        Serial.print("Raw acceleration values (x,y,z): ");
-        Serial.print(accRaw.x);
-        Serial.print("   ");
-        Serial.print(accRaw.y);
-        Serial.print("   ");
-        Serial.println(accRaw.z);
 
-        Serial.print("Acceleration values (x,y,z): ");
-        Serial.print(accCorrRaw.x);
-        Serial.print("   ");
-        Serial.print(accCorrRaw.y);
-        Serial.print("   ");
-        Serial.println(accCorrRaw.z);
-
-        Serial.print("g values (x,y,z): ");
+        Serial.print("Acceleration g (x,y,z): ");
         Serial.print(gValue.x);
         Serial.print("   ");
         Serial.print(gValue.y);
         Serial.print("   ");
-        Serial.println(gValue.z);
+        Serial.print(gValue.z);
+        Serial.print("   ");
+        Serial.print("Result g: ");
+        Serial.println(resultantG);
 
-        Serial.print("Resultant g: ");
-        Serial.println(resultantG); // should always be 1 g if only gravity acts on the sensor.
+        Serial.print("Gyroscope degrees/s: ");
+        Serial.print(gyr.x);
+        Serial.print("   ");
+        Serial.print(gyr.y);
+        Serial.print("   ");
+        Serial.println(gyr.z);
+
+        Serial.print("Magnetometer µTesla: ");
+        Serial.print(magValue.x);
+        Serial.print("   ");
+        Serial.print(magValue.y);
+        Serial.print("   ");
+        Serial.println(magValue.z);
+
+        Serial.print("Temperature in °C: ");
+        Serial.println(temp);
         Serial.println();
         delay(1000);
       }
